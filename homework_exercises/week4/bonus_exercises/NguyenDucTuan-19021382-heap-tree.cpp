4d4dd5ad360f703086c95a1e792deffc98828484
NguyenDucTuanUET <64391055+ductuanUET@users.noreply.github.com>
2023-10-17 23:14:53 +0700

    lint code

--- homework_exercises/week4/bonus_exercises/NguyenDucTuan-19021382-heap-tree.cpp
+++ homework_exercises/week4/bonus_exercises/NguyenDucTuan-19021382-heap-tree.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 class MaxHeap
@@ -8,26 +8,32 @@ class MaxHeap
     int heap_size;
 
 public:
-    MaxHeap(int _capacity) {
+    MaxHeap(int _capacity)
+    {
         capacity = _capacity;
         root = new int[capacity];
         heap_size = 0;
     }
 
-    int getParent(int i) {
-        return (i - 1)/2;
+    int getParent(int i)
+    {
+        return (i - 1) / 2;
     }
 
-    int getLeftChild(int i) {
-        return (2*i + 1);
+    int getLeftChild(int i)
+    {
+        return (2 * i + 1);
     }
 
-    int getRightChild(int i) {
-        return (2*i + 2);
+    int getRightChild(int i)
+    {
+        return (2 * i + 2);
     }
 
-    void insert(int value) {
-        if (heap_size >= capacity) {
+    void insert(int value)
+    {
+        if (heap_size >= capacity)
+        {
             cout << "Heap is full." << endl;
             return;
         }
@@ -36,32 +42,39 @@ public:
         root[i] = value;
         heap_size++;
 
-        while (i != 0 && root[getParent(i)] < root[i]) {
+        while (i != 0 && root[getParent(i)] < root[i])
+        {
             swap(root[i], root[getParent(i)]);
             i = getParent(i);
         }
     }
 
-    void MaxHeapify(int i) {
+    void MaxHeapify(int i)
+    {
         int left = getLeftChild(i);
         int right = getRightChild(i);
         int largest = i;
 
-        if (left < heap_size && root[left] > root[i]) {
+        if (left < heap_size && root[left] > root[i])
+        {
             largest = left;
         }
-        if (right < heap_size && root[right] > root[largest]) {
+        if (right < heap_size && root[right] > root[largest])
+        {
             largest = right;
         }
 
-        if (largest != i) {
+        if (largest != i)
+        {
             swap(root[i], root[largest]);
             MaxHeapify(largest);
         }
     }
 
-    int getMaxValue() {
-        if (heap_size <= 0) {
+    int getMaxValue()
+    {
+        if (heap_size <= 0)
+        {
             cout << "Heap is empty" << endl;
             return -1;
         }
@@ -69,17 +82,21 @@ public:
         return root[0];
     }
 
-    int getHeapSize() {
+    int getHeapSize()
+    {
         return heap_size;
     }
 
-    int extractMaxValue() {
-        if (heap_size <= 0) {
+    int extractMaxValue()
+    {
+        if (heap_size <= 0)
+        {
             cout << "Heap is empty." << endl;
             return -1;
         }
 
-        if (heap_size == 1) {
+        if (heap_size == 1)
+        {
             heap_size--;
             return root[0];
         }
@@ -87,21 +104,25 @@ public:
         int max = root[0];
         root[0] = root[heap_size - 1];
         heap_size--;
-        
+
         MaxHeapify(0);
 
         return max;
     }
 
-    void removeNode(int value) {
+    void removeNode(int value)
+    {
         int i;
 
-        for (i = 0; i < heap_size; i++) {
-            if (root[i] == value) {
+        for (i = 0; i < heap_size; i++)
+        {
+            if (root[i] == value)
+            {
                 break;
             }
         }
-        if(i == heap_size) {
+        if (i == heap_size)
+        {
             cout << "Not found";
             return;
         }
@@ -109,7 +130,8 @@ public:
         root[i] = root[heap_size - 1];
         heap_size--;
 
-        while(i > 0 && root[i] > root[getParent(i)]) {
+        while (i > 0 && root[i] > root[getParent(i)])
+        {
             swap(root[i], root[getParent(i)]);
             i = getParent(i);
         }
@@ -117,12 +139,15 @@ public:
         MaxHeapify(i);
     }
 
-    int depth(int data, int i = 0, int currentDepth = 0) {
-        if (i >= heap_size) {
+    int depth(int data, int i = 0, int currentDepth = 0)
+    {
+        if (i >= heap_size)
+        {
             return -1;
         }
 
-        if (root[i] == data) {
+        if (root[i] == data)
+        {
             return currentDepth;
         }
 
@@ -132,19 +157,23 @@ public:
         int leftDepth = depth(data, leftChildIndex, currentDepth + 1);
         int rightDepth = depth(data, rightChildIndex, currentDepth + 1);
 
-        if (leftDepth != -1) {
+        if (leftDepth != -1)
+        {
             return leftDepth;
         }
 
-        if (rightDepth != -1) {
+        if (rightDepth != -1)
+        {
             return rightDepth;
         }
 
         return -1;
     }
 
-    int numOfLeaves(int i = 0) {
-        if (i >= heap_size || getLeftChild(i) >= heap_size) {
+    int numOfLeaves(int i = 0)
+    {
+        if (i >= heap_size || getLeftChild(i) >= heap_size)
+        {
             return 1;
         }
 
@@ -154,42 +183,49 @@ public:
         return leftLeaves + rightLeaves;
     }
 
-    void printHeapTree() {
-        for (int i = 0; i < heap_size; i++) {
+    void printHeapTree()
+    {
+        for (int i = 0; i < heap_size; i++)
+        {
             cout << root[i] << " ";
         }
         cout << endl;
     }
 
-    int findMaxChild() {
-    int maxChildCount = 0;
-    int nodeWithMaxChildren = -1;
+    int findMaxChild()
+    {
+        int maxChildCount = 0;
+        int nodeWithMaxChildren = -1;
 
-    for (int i = 0; i < heap_size; i++) {
-        int leftChildIndex = getLeftChild(i);
-        int rightChildIndex = getRightChild(i);
-        int childCount = 0;
+        for (int i = 0; i < heap_size; i++)
+        {
+            int leftChildIndex = getLeftChild(i);
+            int rightChildIndex = getRightChild(i);
+            int childCount = 0;
 
-        if (leftChildIndex < heap_size && root[leftChildIndex] > root[i]) {
-            childCount++;
+            if (leftChildIndex < heap_size && root[leftChildIndex] > root[i])
+            {
+                childCount++;
+            }
+
+            if (rightChildIndex < heap_size && root[rightChildIndex] > root[i])
+            {
+                childCount++;
+            }
+
+            if (childCount > maxChildCount)
+            {
+                maxChildCount = childCount;
+                nodeWithMaxChildren = i;
+            }
         }
 
-        if (rightChildIndex < heap_size && root[rightChildIndex] > root[i]) {
-            childCount++;
-        }
-
-        if (childCount > maxChildCount) {
-            maxChildCount = childCount;
-            nodeWithMaxChildren = i;
-        }
+        return nodeWithMaxChildren;
     }
-
-    return nodeWithMaxChildren;
-    }
-
 };
 
-int main() {
+int main()
+{
     MaxHeap maxHeap(10);
     maxHeap.insert(4);
     maxHeap.insert(10);
